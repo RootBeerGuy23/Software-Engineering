5ba255a5f2d5783c8bf95cc258b452ae6e211beb
Christopher <christopher023@binus.ac.id>
2024-05-13 16:58:01 +0700

    aset

---
+++ Auth/Login.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../asset/css/style.css">
+    <link rel="stylesheet" href="../asset/css/bootstrap.min.css">
+    <title>BeeLife</title>
+</head>
+
+<body>
+    <section>
+        <div class="login">
+            <div class="login-page">
+                <div class="login-form">
+                    <h2 class="text-center">Log In</h2>
+                    <form>
+                        <!-- <div class="alert alert-danger failed-message">Password Doesn't Match</div> -->
+                        <div class="inputBox">
+                            <!-- <label>E-Mail</label> -->
+                            <input type="email" class="form-control" name="email" id="email"
+                                placeholder="Username">
+                        </div>
+                        <div class="inputBox">
+                            <!-- <label>Password</label> -->
+                            <input type="password" class="form-control" name="password" id="password"
+                                placeholder="Password">
+                        </div>
+                        <div class="submit">
+                            <button type="submit" class="btn">Sign In</button>
+                        </div>
+                        <div class="forget-link text-center">
+                            <a href="#">Forgot Password?</a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
